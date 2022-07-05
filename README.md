0e11e12b4d8842f2d25651c301233dbabccd3ea4
azzireluto <bruno.a@outlook.com.br>
2022-07-05 17:18:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Azzi BoookStores
+
+Repositório do curso de [FastAPI rest](https://www.udemy.com/course/fastapi-rest/)  Ministrado pela Ines Ivanova
+
+
+### 📋 Pré-requisitos
+
+Para conseguir implementar este software são necessários os seguintes itens:
+
+```
+Python 3.10 ou superior
+Pip 22.1 ou superior
+pycharm community edition
+PostgreSQL e phppgAdmin
+```
+
---
+++ main.py
@@ -0,0 +1,49 @@
+import databases
+import sqlalchemy
+from fastapi import FastAPI, Request
+
+DATABASE_URL = "postgresql://azzi:Bu.62991881@localhost/store"
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+books = sqlalchemy.Table(
+    'books',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column('title', sqlalchemy.String),
+    sqlalchemy.Column('author', sqlalchemy.String),
+    sqlalchemy.Column('pages', sqlalchemy.Integer),
+)
+
+# engine = sqlalchemy.create_engine(DATABASE_URL)
+# metadata.create_all(engine)
+
+app = FastAPI()
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    await database.disconnect()
+
+
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+
+@app.get('/books/')
+async def get_all_books():
+    query = books.select()
+    return await database.fetch_all(query)
+
+
+@app.post('/books/')
+async def create_book(request: Request):
+    data = await request.json()
+    query = books.insert().values(**data)
+    last_record_id = await database.execute(query)
+    return {'id': last_record_id}
+
+
+# stop in 3-17
